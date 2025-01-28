f06e774110b01ea5a9bd467821e4c3ce6849b643
R-Alejandro-U <alejandro.urdaneta2314@gmail.com>
2025-01-27 23:55:48 -0300

    cors implementado

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import  cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cors());
+  app.enableCors();
   setupSwagger(app);
   await app.listen(process.env.PORT ?? 3000, () =>
     console.log(`Server is listen on the port ${process.env.PORT}`),

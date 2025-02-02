4d876aacc2e1ab7378d871f2f61083de6579615d
R-Alejandro-U <alejandro.urdaneta2314@gmail.com>
2025-02-02 12:49:36 -0300

    .

--- src/Seeder/seeder.module.ts
+++ src/Seeder/seeder.module.ts
@@ -1,9 +1,13 @@
 import { Module, OnApplicationBootstrap } from '@nestjs/common';
 import { SeeederDB } from './seeder.service';
 import { UserModule } from 'src/User/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Category } from 'src/Entities/Category.entity';
+import { Product } from 'src/Entities/Product.entity';
+import { User } from 'src/Entities/User.entity';
 
 @Module({
   providers: [SeeederDB],
-  imports: [UserModule],
+  imports: [UserModule, TypeOrmModule.forFeature([Category, Product, User])],
 })
 export class SeederModule{}
